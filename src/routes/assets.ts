bdff7a7dbe47645f8e4ddf22dada4a84ee6d6277
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2024-06-13 16:45:29 -0500

    Allow to query assets endpoint without tag param

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -5,20 +5,26 @@ const WINDOW_SIZE = 10;
 export default async function(req: Request) {
     const url = new URL(req.url);
     const tag = url.searchParams.get("tag");
-    const offset = parseInt(url.searchParams.get("offset") ?? '0')
+    const offset = parseInt(url.searchParams.get("offset") ?? '0');
 
-    if(!tag) return new Response('Missing tag parameter', { status: 403 });
+    if(tag) {
+        var result = await sql`
+        SELECT assets.title, assets.description, assets.uploader, assets.resource_url
+        FROM tag_bridge 
+        INNER JOIN assets ON tag_bridge.asset = assets.id
+        WHERE tag_bridge.tag = ${encodeURIComponent(tag)}
+        ORDER BY assets.created_at OFFSET ${offset * WINDOW_SIZE} LIMIT ${WINDOW_SIZE};`;
+    } else {
+        var result = await sql`
+        SELECT assets.title, assets.description, assets.uploader, assets.resource_url
+        FROM tag_bridge 
+        INNER JOIN assets ON tag_bridge.asset = assets.id
+        ORDER BY assets.created_at OFFSET ${offset * WINDOW_SIZE} LIMIT ${WINDOW_SIZE};`;
+    }
 
-    const res = await sql`
-    SELECT assets.title, assets.description, assets.uploader, assets.resource_url
-    FROM tag_bridge 
-    INNER JOIN assets ON tag_bridge.asset = assets.id
-    WHERE tag_bridge.tag = ${encodeURIComponent(tag)}
-    ORDER BY assets.created_at OFFSET ${offset * WINDOW_SIZE} LIMIT ${WINDOW_SIZE};`
-    
-    const text = res.reduce((acc, row) => {
+    const text = result.reduce((acc, row) => {
         return `${acc}${Object.values(row).reduce((a, v) => a + v + ',', '')};`;
-    }, '') 
+    }, '');
     
     return new Response(text);
 }
