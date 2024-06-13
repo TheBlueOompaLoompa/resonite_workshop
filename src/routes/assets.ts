27eeceeca1a99479aee0cff5953438e63f5a6947
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2024-06-13 01:50:33 -0500

    Include offset parameter in **assets** endpoint

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -1,14 +1,20 @@
 import sql from "$lib/db";
 
+const WINDOW_SIZE = 10;
+
 export default async function(req: Request) {
     const url = new URL(req.url);
     const tag = url.searchParams.get("tag");
+    const offset = parseInt(url.searchParams.get("offset") ?? '0')
 
     if(!tag) return new Response('Missing tag parameter', { status: 403 });
 
     const res = await sql`
-    select assets.title, assets.description, assets.uploader, assets.resource_url from tag_bridge 
-    inner join assets on tag_bridge.asset=assets.id where tag_bridge.tag=${encodeURIComponent(tag)} limit 10;`
+    SELECT assets.title, assets.description, assets.uploader, assets.resource_url
+    FROM tag_bridge 
+    INNER JOIN assets ON tag_bridge.asset=assets.id
+    WHERE tag_bridge.tag=${encodeURIComponent(tag)}
+    ORDER BY assets.created_at OFFSET ${offset * WINDOW_SIZE} LIMIT ${WINDOW_SIZE};`
     
     const text = res.reduce((acc, row) => {
         return `${acc}${Object.values(row).reduce((a, v) => a + v + ',', '')};`;
