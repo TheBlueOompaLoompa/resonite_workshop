e0a39f11255c27a3c003469999862ad55311107e
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2024-06-13 01:54:53 -0500

    Reformat response function for edge cases

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,24 @@ const server = Bun.serve({
         const uri = new URL(req.url);
 
         const res = await response(uri, req);
-
-        if(res) {
-            console.log(`${res.status} ${req.method} ${uri.pathname}`);
-            return res;
-        }
-
-        return new Response(`This route doesn't exist`, {
-            status: 404
-        });
+        console.log(`${res.status} ${req.method} ${uri.pathname}`);
+        return res;
     },
     hostname: process.env.HOST ?? '0.0.0.0',
     port: parseInt(process.env.PORT ?? '3000')
 });
 
-async function response(url: import('url').URL, req: Request): Promise<Response | null> {
+async function response(url: import('url').URL, req: Request): Promise<Response> {
     switch(url.pathname) {
     case "/tags":
         return await Tags(req);
     case "/assets":
         return await Assets(req);
-    default:
-        break;
     }
-
-    return null;
+    
+    return new Response(`This route doesn't exist`, {
+        status: 404
+    });
 }
 
 console.log(`Hosting Resonite Workshop at ${server.url.href}`);
